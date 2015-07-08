36e3de100d6219e225e557fd98c9ccd967c46129
Bill Fogg <BillFogg@Bills-MacBook-Pro.local>
2015-07-07 22:05:17 -0400

    Built initial validations for title, author and serial_number but unable to validate date selections

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ActiveRecord::Base
+	validates :title, presence: true
+	validates :author, presence: true
+	validates :serial_number, presence: true
+	#validates :publication_date, presence: true
+	# validates_date :publication_date, presence: true
+	validates :publication_date, timeliness: { allow_nil: false, allow_blank: false }
 end
--- spec/features/books/create_spec.rb
+++ spec/features/books/create_spec.rb
@@ -16,4 +16,144 @@ describe "creating a new book" do
 
 		expect(page).to have_content "Book was successfully created"
 	end
+
+	it "displays an error when the book has no title" do 
+		expect(Book.count).to eq(0)
+
+
+		visit "/books"
+		click_link "New Book"
+		expect(page).to have_content("New Book")
+
+		fill_in "Title", with: ""
+		fill_in "Author", with: "Test Author"
+		select "2010", from: "book[publication_date(1i)]"
+		select "June", from: "book[publication_date(2i)]"
+		select "15", from: "book[publication_date(3i)]"
+		fill_in "Serial number", with: "0123456789"
+		click_button "Create Book"
+
+		expect(page).to have_content("error")
+		expect(Book.count).to eq(0)
+
+		visit "/books"
+		expect(page).to_not have_content "Book was successfully created"
+	end
+
+	it "displays an error when the book has no author" do 
+		expect(Book.count).to eq(0)
+
+
+		visit "/books"
+		click_link "New Book"
+		expect(page).to have_content("New Book")
+
+		fill_in "Title", with: "Test Title"
+		fill_in "Author", with: ""
+		select "2010", from: "book[publication_date(1i)]"
+		select "June", from: "book[publication_date(2i)]"
+		select "15", from: "book[publication_date(3i)]"
+		fill_in "Serial number", with: "0123456789"
+		click_button "Create Book"
+
+		expect(page).to have_content("error")
+		expect(Book.count).to eq(0)
+
+		visit "/books"
+		expect(page).to_not have_content "Book was successfully created"
+	end
+
+	it "displays an error when the book has no serial number" do 
+		expect(Book.count).to eq(0)
+
+
+		visit "/books"
+		click_link "New Book"
+		expect(page).to have_content("New Book")
+
+		fill_in "Title", with: "Test Title"
+		fill_in "Author", with: "Test Author"
+		select "2010", from: "book[publication_date(1i)]"
+		select "June", from: "book[publication_date(2i)]"
+		select "15", from: "book[publication_date(3i)]"
+		fill_in "Serial number", with: ""
+		click_button "Create Book"
+
+		expect(page).to have_content("error")
+		expect(Book.count).to eq(0)
+
+		visit "/books"
+		expect(page).to_not have_content "Book was successfully created"
+	end
+
+	# it "displays an error when the book has no month selected" do 
+	# 	expect(Book.count).to eq(0)
+
+
+	# 	visit "/books"
+	# 	click_link "New Book"
+	# 	expect(page).to have_content("New Book")
+
+	# 	fill_in "Title", with: "Test Title"
+	# 	fill_in "Author", with: "Test Author"
+	# 	select "2010", from: "book[publication_date(1i)]"
+	# 	select "", from: "book[publication_date(2i)]"
+	# 	select "15", from: "book[publication_date(3i)]"
+	# 	fill_in "Serial number", with: "0123456789"
+	# 	click_button "Create Book"
+
+	# 	expect(page).to have_content("error")
+	# 	expect(Book.count).to eq(0)
+
+	# 	visit "/books"
+	# 	expect(page).to_not have_content "Book was successfully created"
+	# end
+
+	# it "displays an error when the book has no day selected" do 
+	# 	expect(Book.count).to eq(0)
+
+
+	# 	visit "/books"
+	# 	click_link "New Book"
+	# 	expect(page).to have_content("New Book")
+
+	# 	fill_in "Title", with: "Test Title"
+	# 	fill_in "Author", with: "Test Author"
+	# 	select "2010", from: "book[publication_date(1i)]"
+	# 	select "June", from: "book[publication_date(2i)]"
+	# 	select "", from: "book[publication_date(3i)]"
+	# 	fill_in "Serial number", with: "0123456789"
+	# 	click_button "Create Book"
+
+	# 	expect(page).to have_content("error")
+	# 	expect(Book.count).to eq(0)
+
+	# 	visit "/books"
+	# 	expect(page).to_not have_content "Book was successfully created"
+	# end
+
+	# it "displays an error when the book has no year selected" do 
+	# 	expect(Book.count).to eq(0)
+
+
+	# 	visit "/books"
+	# 	click_link "New Book"
+	# 	expect(page).to have_content("New Book")
+
+	# 	fill_in "Title", with: "Test Title"
+	# 	fill_in "Author", with: "Test Author"
+	# 	select "", from: "book[publication_date(1i)]"
+	# 	select "June", from: "book[publication_date(2i)]"
+	# 	select "15", from: "book[publication_date(3i)]"
+	# 	fill_in "Serial number", with: "0123456789"
+	# 	click_button "Create Book"
+
+	# 	expect(page).to have_content("error")
+	# 	expect(Book.count).to eq(0)
+
+	# 	visit "/books"
+	# 	expect(page).to_not_have_content "Book was successfully created"
+	# end
+
+	
 end

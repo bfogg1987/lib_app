667ec573dca475a44544a1eb22f99449872196fc
Bill Fogg <BillFogg@Bills-MacBook-Pro.local>
2015-07-14 15:20:04 -0400

    added length validation for title, author and serial number as well as added devise user functionality

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ActiveRecord::Base
 	validates :title, presence: true
+	validates :title, length: {minimum: 3}
 	validates :author, presence: true
+	validates :author, length: {minimum: 3}
 	validates :serial_number, presence: true
+	validates :serial_number, length: {minimum: 6}
 	#validates :publication_date, presence: true
 	# validates_date :publication_date, presence: true
 	validates :publication_date, timeliness: { allow_nil: false, allow_blank: false }
--- spec/features/books/create_spec.rb
+++ spec/features/books/create_spec.rb
@@ -85,75 +85,74 @@ describe "creating a new book" do
 		visit "/books"
 		expect(page).to_not have_content "Book was successfully created"
 	end
-
-	# it "displays an error when the book has no month selected" do 
-	# 	expect(Book.count).to eq(0)
-
-
-	# 	visit "/books"
-	# 	click_link "New Book"
-	# 	expect(page).to have_content("New Book")
-
-	# 	fill_in "Title", with: "Test Title"
-	# 	fill_in "Author", with: "Test Author"
-	# 	select "2010", from: "book[publication_date(1i)]"
-	# 	select "", from: "book[publication_date(2i)]"
-	# 	select "15", from: "book[publication_date(3i)]"
-	# 	fill_in "Serial number", with: "0123456789"
-	# 	click_button "Create Book"
-
-	# 	expect(page).to have_content("error")
-	# 	expect(Book.count).to eq(0)
-
-	# 	visit "/books"
-	# 	expect(page).to_not have_content "Book was successfully created"
-	# end
-
-	# it "displays an error when the book has no day selected" do 
-	# 	expect(Book.count).to eq(0)
-
-
-	# 	visit "/books"
-	# 	click_link "New Book"
-	# 	expect(page).to have_content("New Book")
-
-	# 	fill_in "Title", with: "Test Title"
-	# 	fill_in "Author", with: "Test Author"
-	# 	select "2010", from: "book[publication_date(1i)]"
-	# 	select "June", from: "book[publication_date(2i)]"
-	# 	select "", from: "book[publication_date(3i)]"
-	# 	fill_in "Serial number", with: "0123456789"
-	# 	click_button "Create Book"
-
-	# 	expect(page).to have_content("error")
-	# 	expect(Book.count).to eq(0)
-
-	# 	visit "/books"
-	# 	expect(page).to_not have_content "Book was successfully created"
-	# end
-
-	# it "displays an error when the book has no year selected" do 
-	# 	expect(Book.count).to eq(0)
-
-
-	# 	visit "/books"
-	# 	click_link "New Book"
-	# 	expect(page).to have_content("New Book")
-
-	# 	fill_in "Title", with: "Test Title"
-	# 	fill_in "Author", with: "Test Author"
-	# 	select "", from: "book[publication_date(1i)]"
-	# 	select "June", from: "book[publication_date(2i)]"
-	# 	select "15", from: "book[publication_date(3i)]"
-	# 	fill_in "Serial number", with: "0123456789"
-	# 	click_button "Create Book"
-
-	# 	expect(page).to have_content("error")
-	# 	expect(Book.count).to eq(0)
-
-	# 	visit "/books"
-	# 	expect(page).to_not_have_content "Book was successfully created"
-	# end
-
 	
+	it "displays an error when the book has a title less than 3 characters" do 
+		expect(Book.count).to eq(0)
+
+
+		visit "/books"
+		click_link "New Book"
+		expect(page).to have_content("New Book")
+
+		fill_in "Title", with: "ab"
+		fill_in "Author", with: "Test Author"
+		select "2010", from: "book[publication_date(1i)]"
+		select "June", from: "book[publication_date(2i)]"
+		select "15", from: "book[publication_date(3i)]"
+		fill_in "Serial number", with: "012345"
+		click_button "Create Book"
+
+		expect(page).to have_content("error")
+		expect(Book.count).to eq(0)
+
+		visit "/books"
+		expect(page).to_not have_content "Book was successfully created"
+	end
+
+	it "displays an error when the book has an author less than 3 characters" do 
+		expect(Book.count).to eq(0)
+
+
+		visit "/books"
+		click_link "New Book"
+		expect(page).to have_content("New Book")
+
+		fill_in "Title", with: "abc"
+		fill_in "Author", with: "ab"
+		select "2010", from: "book[publication_date(1i)]"
+		select "June", from: "book[publication_date(2i)]"
+		select "15", from: "book[publication_date(3i)]"
+		fill_in "Serial number", with: "012345"
+		click_button "Create Book"
+
+		expect(page).to have_content("error")
+		expect(Book.count).to eq(0)
+
+		visit "/books"
+		expect(page).to_not have_content "Book was successfully created"
+	end
+
+	it "displays an error when the book has a serial number less than 6 characters" do 
+		expect(Book.count).to eq(0)
+
+
+		visit "/books"
+		click_link "New Book"
+		expect(page).to have_content("New Book")
+
+		fill_in "Title", with: "Test Title"
+		fill_in "Author", with: "Test Author"
+		select "2010", from: "book[publication_date(1i)]"
+		select "June", from: "book[publication_date(2i)]"
+		select "15", from: "book[publication_date(3i)]"
+		fill_in "Serial number", with: "01234"
+		click_button "Create Book"
+
+		expect(page).to have_content("error")
+		expect(Book.count).to eq(0)
+
+		visit "/books"
+		expect(page).to_not have_content "Book was successfully created"
+	end
+
 end
